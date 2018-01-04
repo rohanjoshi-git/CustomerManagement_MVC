eac9b2c4b7d3805de0ce4f46f5d328780f12365f
Rohan <Rohan@DESKTOP-G7GO59L>
2018-01-04 19:23:00 +1300

    SearchCustomer, DisplayCustomer (Partial View and WebGrid)

--- CustomerManagement/Controllers/CustomerController.cs
+++ CustomerManagement/Controllers/CustomerController.cs
@@ -38,20 +38,68 @@ namespace CustomerManagement.Controllers
 
             return View("EnterCustomer", obj);
         }
-        public ActionResult Submit(Customer obj) // validation runs
+
+        public ActionResult EnterSearch()
         {
-            if(ModelState.IsValid)
+            // View Model object
+            CustomerViewModel obj = new CustomerViewModel();
+            obj.customers = new List<Customer>();
+            return View("SearchCustomer", obj);
+        }
+
+        public ActionResult SearchCustomer()
+        {
+            // View Model object
+            CustomerViewModel obj = new CustomerViewModel();
+
+            // fill the customers collections
+            CustomerDal dal = new CustomerDal();
+            // to fill all customers
+            // List<Customer> customersColl = dal.Customers.ToList<Customer>();
+
+            string str = Request.Form["txtCustomerName"].ToString();
+
+            // to filter customers with conditions
+            List<Customer> customersColl
+                = (from x in dal.Customers
+                   where x.CustomerName == str
+                   select x).ToList<Customer>();
+            obj.customers = customersColl;
+
+            return View("SearchCustomer", obj);
+        }
+
+        public ActionResult Submit() // validation runs
+        {
+
+            CustomerViewModel vm = new CustomerViewModel();
+
+            // manual binding of object with form elements
+            Customer obj = new Customer();
+            obj.CustomerName = Request.Form["customer.CustomerName"];
+            obj.CustomerCode = Request.Form["customer.CustomerCode"];
+
+            if (ModelState.IsValid)
             {
                 // insert the Customer object to database
                 // EF DAL
                 CustomerDal Dal = new CustomerDal();
                 Dal.Customers.Add(obj); // in memory
                 Dal.SaveChanges(); // physical commit
+                vm.customer = new Customer(); // to refresh the view with new emply object
 
                 //return View("Customer", obj);
             }
-            
-                return View("EnterCustomer",obj);            
+            else
+            {
+                vm.customer = obj;  // if the object is not valid, persist the object and show it to user
+            }
+            // fill the customers collections
+            CustomerDal dal = new CustomerDal();
+            List<Customer> customersColl = dal.Customers.ToList<Customer>();
+            vm.customers = customersColl;
+
+            return View("EnterCustomer",vm);            
         }
     }
 }
