c2491d8f457316f3dd5895fec89d45d4c7bd6d3a
Rohan <Rohan@DESKTOP-G7GO59L>
2018-01-21 21:17:14 +1300

    Implemented Update and Delete methods using WebAPI

--- CustomerManagement/Controllers/CustomerAPIController.cs
+++ CustomerManagement/Controllers/CustomerAPIController.cs
@@ -11,6 +11,7 @@ namespace CustomerManagement.Controllers
 {
     public class CustomerController : ApiController
     {
+        // Insert
         public List<Customer> Post(Customer objCustomer)
         {
             if (ModelState.IsValid)
@@ -28,15 +29,46 @@ namespace CustomerManagement.Controllers
 
             return customersColl; // WebAOI will decide the content type on its own (HTML, JSON, JPEG...)
         }
-  
+
+        // Select
         public List<Customer> Get()
         {
+            // Read the query string
+            var allUrlKeyValues = ControllerContext.Request.GetQueryNameValuePairs();
+
+            string customerCode = allUrlKeyValues.SingleOrDefault(x => x.Key == "CustomerCode").Value;
+            string customerName = allUrlKeyValues.SingleOrDefault(x => x.Key == "CustomerName").Value;
+
             // fill the customers collections
             Dal.Dal dal = new Dal.Dal();
-            List<Customer> customersColl = dal.Customers.ToList<Customer>();
+            List<Customer> customersColl = new List<Customer>();
+
+            if (customerName != null)
+            {
+                // Select the record using LINQ (used in Serch Customer Screen)
+                // fill the customers collections
+                customersColl = (from c in dal.Customers
+                                                where c.CustomerName == customerName
+                                 select c).ToList<Customer>();
+            }
+
+            else if (customerCode != null)
+            {
+                customersColl = (from c in dal.Customers
+                                 where c.CustomerCode == customerCode
+                                 select c).ToList<Customer>();
+            }
+
+            else
+            {
+                customersColl = dal.Customers.ToList<Customer>();
+            }
+            
             return customersColl;
         }
 
+        /* // Start - commented as it will be handled in one Get method
+        // Select (used in Serch Customer Screen
         public List<Customer> Get(string CustomerName)  // parameter name should match the query string parameters
         {
             // fill the customers collections
@@ -45,6 +77,40 @@ namespace CustomerManagement.Controllers
                                             where c.CustomerName == CustomerName
                                             select c).ToList<Customer>();
             return customersColl;
+        }
+        */ // End - commented as it will be handled in one Get method
+
+        // Update
+        public List<Customer> Put(Customer objCustomer)
+        {
+            // Select the record using LINQ
+            Dal.Dal objDal = new Dal.Dal();
+            Customer custUpdate = (from c in objDal.Customers
+                                   where c.CustomerCode == objCustomer.CustomerCode
+                                   select c).ToList<Customer>()[0];
+
+            // Update the record
+            custUpdate.CustomerName = objCustomer.CustomerName;
+            custUpdate.CustomerAmount = objCustomer.CustomerAmount;
+
+            List<Customer> customerColl = objDal.Customers.ToList<Customer>();
+            return customerColl;
+        }
+
+        // Delete
+        public List<Customer> Delete(Customer objCustomer)
+        {
+            // Select the record using LINQ
+            Dal.Dal objDal = new Dal.Dal();
+            Customer custDelete = (from c in objDal.Customers
+                                   where c.CustomerCode == objCustomer.CustomerCode
+                                   select c).ToList<Customer>()[0];
+
+            objDal.Customers.Remove(custDelete);
+            objDal.SaveChanges();
+
+            List<Customer> customerColl = objDal.Customers.ToList<Customer>();
+            return customerColl;
         }
     }
 }
--- CustomerManagement/Scripts/Customer.js
+++ CustomerManagement/Scripts/Customer.js
@@ -57,6 +57,48 @@ function CustomerViewModel($scope, $http)  // $scope --> for scope management, $
         });
     }
 
+    // Update Customer
+    $scope.Update = function () {
+        // make a call to server to Update data
+        $http({ method: "PUT", data: $scope.Customer, url: "/Api/Customer" /*"Submit"*/ }).then(function (successData, status, headers, config) {
+            // Load the collection of Customer
+            $scope.Customers = successData.data;
+
+            // Clear text boxes
+            $scope.Customer =
+                {
+                    "CustomerCode": "",
+                    "CustomerName": "",
+                    "CustomerAmount": "",
+                    "CustomerAmountColor": ""
+                };
+        });
+    }
+
+    // Delete Customer
+    $scope.Delete = function () {
+
+        // Add this line of code in order to work Delete method
+        $http.defaults.headers["delete"] = {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+
+        // make a call to server to Delete data
+        $http({ method: "DELETE", data: $scope.Customer, url: "/Api/Customer" /*"Submit"*/ }).then(function (successData, status, headers, config) {
+            // Load the collection of Customer
+            $scope.Customers = successData.data;
+
+            // Clear text boxes
+            $scope.Customer =
+                {
+                    "CustomerCode": "",
+                    "CustomerName": "",
+                    "CustomerAmount": "",
+                    "CustomerAmountColor": ""
+                };
+        });
+    }
+
     // Load Cutomer data for all customers
     $scope.Load = function () {
         $http({ method: "GET", url: "/Api/Customer" /*"getCustomers"*/ }).then(function (successData, status, headers, config) {
@@ -77,7 +119,24 @@ function CustomerViewModel($scope, $http)  // $scope --> for scope management, $
         // use params to pass data to server in GET method OR use POST method
     }
 
+    // Load Cutomer data for the entered Customer Code
+    $scope.LoadByCode = function (CustomerCode) {
+
+        //var objCustomer = $scope.Customer;  // no need to pass object as data is being sent by QueryString
+
+        $http({ method: "GET", /*params: objCustomer,*/ url: "/Api/Customer?CustomerCode=" + CustomerCode /*"getCustomerByCode"*/ }).then(function (successData, status, headers, config) {
+            // Load the collection of Customer
+            $scope.Customers = successData.data;
+
+            // bind the Customer data to text boxes
+            $scope.Customer = $scope.Customers[0];
+
+            // get list of all customers and bind to table
+            $scope.Load();
+        });
+        // use params to pass data to server in GET method OR use POST method
+    }
+
     // call function to load Customer data
     $scope.Load();
-
 }
